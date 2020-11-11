cb53570d67b7a9357a22b2b2012e7b53f095ec73
+ <+>
2020-11-10 20:16:02 -0500

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
-  # your code here
+  new_array = []
+  array.each do |number|
+    new_array << (number ** 2)
+  end
+  return new_array
+end
+
+def array_collect(array)
+  array.collect{|number| number*2}
 end
